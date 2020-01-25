2ef237eb81d1ba0753c448749d8f48642a99da36
Momoko Kawata <momoko.k.0421@gmail.com>
2020-01-25 22:18:15 +0900

    Add file as script which as home_dir

--- change_home_dir_name.sh
+++ change_home_dir_name.sh
@@ -2,7 +2,7 @@
 file1="sfm_openmvg.sh"
 file2="openmvg_bin2json.sh"
 file3="find_scale_pointcloud.py"
-
+file4="main.m"
 function change_dir_name () {
     echo "$1"
     cat "$1" | sed -e 's#home_dir\s*=\s*".*"#home_dir="'$2'"#g' > tmp | mv tmp "$1"
@@ -11,4 +11,5 @@ echo "New Directory Name : '$1'" \
 && change_dir_name $file1 $1 \
 && change_dir_name $file2 $1 \
 && change_dir_name $file3 $1 \
+&& chnage_dir_name $file4 $1 \
 && chmod +x *.sh
