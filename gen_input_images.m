58b5c9aa5b0bd597f4f4885862d096195d501424
Momoko Kawata <momoko.k.0421@gmail.com>
2020-03-09 15:53:52 +0900

    Generate new directory for haikeisabunn no nolaser image

--- gen_input_images.m
+++ gen_input_images.m
@@ -1,6 +1,7 @@
 function gen_input_images()
-    move_sfm_input();
+    % move_sfm_input();
     move_laser_extraction_input();
+    move_nolaser_extraction_input();
 end
 
 function move_sfm_input()
@@ -22,6 +23,7 @@ function move_sfm_input()
 end
 
 function move_laser_extraction_input()
+% for laser extraction
     cnt = 0;
     imgs = dir('./images/original/eqr_img_90deg/');
 
@@ -31,6 +33,25 @@ function move_laser_extraction_input()
             if rem(num,2) ~= 0 % even number's images are images for sfm
                 input = [imgs(i).folder, '/', imgs(i).name];
                 output = ['./images/laser_detection/L', num2str(cnt, '%05.f'), '.JPG'];
+                command = sprintf("cp %s %s", input, output);
+                system(command);
+                cnt = cnt + 1;
+            end
+        end
+    end
+end
+
+function move_nolaser_extraction_input()
+    % for laser extraction ; no laser image
+    cnt = 0;
+    imgs = dir('./images/original/eqr_img_90deg/');
+
+    for i = 1:length(imgs)
+        if imgs(i).isdir==0 && contains(imgs(i).name, 'JPG')
+            num = sscanf(imgs(i).name, 'R00%d_er.JPG');
+            if rem(num,2) == 0 % even number's images are images for sfm
+                input = [imgs(i).folder, '/', imgs(i).name];
+                output = ['./images/laser_detection/no_laser/N', num2str(cnt, '%05.f'), '.JPG'];
                 command = sprintf("cp %s %s", input, output);
                 system(command);
                 cnt = cnt + 1;
